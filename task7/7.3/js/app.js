b3378da531a2914a6ba9defc0154222c3c71ffd4
Andrey Shelemetev <andrey.shelemetev@gmail.com>
2020-06-22 23:29:03 +0300

    correct 7.3

--- task7/7.3/js/app.js
+++ task7/7.3/js/app.js
@@ -1,1 +1,14 @@
+var titleText = 'Привет, мой друг! Добро пожаловать на сайт группы отелей Selly Hotels!';
 
+var promoTitle = document.getElementById('promoTitle'); // получаем заголовок страницы
+promoTitle.innerText = titleText;                       // заменяем текст в заголовке
+
+var button = document.getElementById('showAllFeedbacks');
+button.addEventListener('click', showAllFeedbacks);
+
+function showAllFeedbacks() {
+    var allfeedbacks = document.getElementById('hiddenFeedbacks');
+    allfeedbacks.classList.remove('hidden');
+    allfeedbacks.classList.add('loaded');
+    button.classList.add('hidden');
+}
